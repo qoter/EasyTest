e113f7c7e869d156a588366743d523f9d3e21d3e
borzov <borzov@skbkontur.ru>
2020-10-20 12:55:34 +0500

    add StreamExtensions and tests

---
+++ src/EasyTest.Tests/Utils/StreamExtensionsTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using EasyTest.Utils;
+using Xunit;
+
+namespace EasyTest.Tests.Utils
+{
+    public class StreamExtensionsTests : IDisposable
+    {
+        private readonly string tempDirectory;
+
+        public StreamExtensionsTests()
+        {
+            tempDirectory = Guid.NewGuid().ToString("N");
+            Directory.CreateDirectory(tempDirectory);
+        }
+
+        public void Dispose()
+        {
+            if (tempDirectory != null && Directory.Exists(tempDirectory))
+                Directory.Delete(tempDirectory, true);
+        }
+
+        [Fact]
+        public void Read_FromEmptyFile_ReturnEmptyStringAndNotCloseStream()
+        {
+            var filePath = Path.Combine(tempDirectory, "test.txt");
+            using (File.Create(filePath)) { }
+            using var stream = File.OpenRead(filePath);
+
+            var str = stream.ReadString();
+            
+            Assert.Equal("", str);
+            Assert.True(stream.CanRead);
+        }
+        
+        [Fact]
+        public void Read_FromFile_ReturnAllContentAndNotCloseStream()
+        {
+            var filePath = Path.Combine(tempDirectory, "test.txt");
+            const string content = "hello\n\tworld!";
+            File.WriteAllText(filePath, content);
+            using var stream = File.OpenRead(filePath);
+
+            var str = stream.ReadString();
+            
+            Assert.Equal(content, str);
+            Assert.True(stream.CanRead);
+        }
+
+        [Fact]
+        public void WriteManyTimes_SuccessWriteAndNotCloseStream()
+        {
+            var filePath = Path.Combine(tempDirectory, "test.txt");
+            using (var stream = File.OpenWrite(filePath))
+            {
+                stream.WriteString("Hello\n");
+                stream.WriteString("World");
+            
+                Assert.True(stream.CanWrite);
+            }
+            Assert.Equal("Hello\nWorld", File.ReadAllText(filePath));
+        }
+    }
+}
---
+++ src/EasyTest/Utils/StreamExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Text;
+
+namespace EasyTest.Utils
+{
+    public static class StreamExtensions
+    {
+        public static string ReadString(this Stream stream, Encoding encoding = null)
+        {
+            encoding ??= Encoding.UTF8;
+
+            var ms = new MemoryStream();
+            stream.CopyTo(ms);
+
+            return encoding.GetString(ms.ToArray());
+        }
+
+        public static void WriteString(this Stream stream, string str, Encoding encoding = null)
+        {
+            encoding ??= Encoding.UTF8;
+
+            var bytes = encoding.GetBytes(str);
+            var ms = new MemoryStream(bytes);
+            ms.CopyTo(stream);
+        }
+    }
+}
