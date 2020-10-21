32e2968b779160d6cd32437de5e2b1eb694da0a3
borzov <borzov@skbkontur.ru>
2020-10-21 11:05:20 +0500

    add more doc in FileContent

--- src/EasyTest/FileContent.cs
+++ src/EasyTest/FileContent.cs
@@ -5,14 +5,28 @@ namespace EasyTest
     [AttributeUsage(AttributeTargets.Property)]
     public class FileContent : Attribute
     {
-        public readonly string FileTemplate;
+        /// <summary>
+        /// File template for search file.
+        /// Can contain a combination of valid literal path, pipe (|) and wildcard (* and ?) characters.
+        /// Pipe can separate different alternatives of files (template like 'fileA|fileB' means search 'fileA' or 'fileB')
+        /// </summary>
+        public string FileTemplate { get; }
 
+        /// <summary>
+        /// Mark content optional, does not throws if file does not exists.
+        /// </summary>
         public bool Optional { get; set; } = false;
 
+        /// <summary>
+        /// Search file in top directories.
+        /// </summary>
         public bool Global { get; set; } = false;
 
+        /// <summary>
+        /// Doesn't deserialize file, write only path to property.
+        /// </summary>
         public bool InjectPath { get; set; } = false;
-
+        
         public FileContent(string fileTemplate)
         {
             FileTemplate = fileTemplate;
